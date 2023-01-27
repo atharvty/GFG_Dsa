ca2f454ac83c2162dbb0a4a8d82fe19694078e51
ATHARV TYAGI <73381314+atharvty@users.noreply.github.com>
2023-01-27 22:47:17 +0530

    Easy on GFG

---
+++ RemoveDuplicatesFromSortedArray.cpp
@@ -0,0 +1,16 @@
+class Solution{
+public:
+    int remove_duplicate(int a[],int n){
+        // code here
+        int i=0;
+        for(int j=1;j<n;j++)
+        {
+            if(a[i]!=a[j])
+            {
+                i++;
+                a[i]=a[j];
+            }
+        }
+        return i+1;
+    }
+};

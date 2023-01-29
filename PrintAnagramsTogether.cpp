19d5aa0dbd3e2d9b904dac124c6048e33e567830
ATHARV TYAGI <73381314+atharvty@users.noreply.github.com>
2023-01-29 15:32:32 +0530

    Medium on GFG

---
+++ PrintAnagramsTogether.cpp
@@ -0,0 +1,27 @@
+class Solution{
+  public:
+    vector<vector<string> > Anagrams(vector<string>& string_list) 
+    {
+         vector<vector<string> > result;
+    
+        // hash map to maintain groups of anagrams
+        unordered_map <string,vector<string> > umap; 
+        
+        for(int i =0; i<string_list.size(); i++ )
+        {
+            string s = string_list[i];
+         
+            // sort each string
+            sort(s.begin(),s.end());
+            
+            // add original string to corresponding sorted string in hash map
+            umap[s].push_back(string_list[i]);
+        }
+        for(auto itr: umap)
+            result.push_back(itr.second);
+            
+       
+        
+        return result;
+    }
+};

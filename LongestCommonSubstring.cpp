001ee3bd974c387acf10058222157fc85a428f7c
ATHARV TYAGI <73381314+atharvty@users.noreply.github.com>
2023-01-31 01:56:07 +0530

    Medium on GFG

---
+++ LongestCommonSubstring.cpp
@@ -0,0 +1,23 @@
+class Solution{
+    public:
+    
+    int longestCommonSubstr (string s1, string s2, int n, int m)
+    {
+        int dp[1001][1001]={0};
+        int ans=0;
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=m;j++)
+            {
+                if(s1[i-1]==s2[j-1])
+                {
+                    dp[i][j]=1+dp[i-1][j-1];
+                    ans=max(ans,dp[i][j]);
+                }
+                else
+                    dp[i][j]=0;
+            }
+        }
+        return ans;
+    }
+};

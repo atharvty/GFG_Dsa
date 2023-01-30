5243e527ebf60c951a8500c239f8ce80e2d6d1e5
ATHARV TYAGI <73381314+atharvty@users.noreply.github.com>
2023-01-31 01:30:20 +0530

    Easy on GFG

---
+++ LevelOrderTraversalInSpiralForm.cpp.cpp
@@ -0,0 +1,33 @@
+vector<int> findSpiral(Node *root)
+{
+    if(root == NULL)
+        return {};
+    vector<int> res;
+    stack<Node*> st1;  
+    stack<Node*> st2;  
+    st1.push(root);
+    while (!st1.empty() || !st2.empty()) {
+        while (!st1.empty()) {
+            Node* temp = st1.top();
+            st1.pop();
+            res.push_back(temp->data);
+ 
+            if (temp->right)
+                st2.push(temp->right);
+            if (temp->left)
+                st2.push(temp->left);
+        }
+        
+        while (!st2.empty()) {
+            Node* temp = st2.top();
+            st2.pop();
+            res.push_back(temp->data);
+ 
+            if (temp->left)
+                st1.push(temp->left);
+            if (temp->right)
+                st1.push(temp->right);
+        }
+    }
+    return res;
+}

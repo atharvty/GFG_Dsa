8be489b8ec432762bcde0c71064f17f6a4bdaa63
ATHARV TYAGI <73381314+atharvty@users.noreply.github.com>
2023-02-06 20:39:28 +0530

    Medium on GFG

---
+++ Count Palindrome Sub-Strings of a String.cpp
@@ -0,0 +1,21 @@
+int CountPS(char S[], int N)
+{
+    vector<vector<int>>dp(N,vector<int>(N,0));
+    int ans= 0;
+    for(int diff= 0;diff<N;diff++){
+        for(int i=0,j=diff;j<N;i++,j++){
+            if(diff==0)dp[i][j]= 1;
+            else if(diff==1){
+                if(S[i]==S[j])dp[i][j]= 1;
+               }
+            else{
+                if(S[i]==S[j])dp[i][j]= dp[i+1][j-1];
+             }
+            if(dp[i][j]==1)ans++;
+        }
+    }
+    return ans- N;
+    //code here
+    
+    
+}

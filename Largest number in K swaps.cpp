bea65c60522d824008e300cd9113ddfefeb30025
ATHARV TYAGI <73381314+atharvty@users.noreply.github.com>
2023-02-18 22:07:43 +0530

    Medium on GFG

---
+++ Largest number in K swaps.cpp
@@ -0,0 +1,29 @@
+
+
+class Solution
+{
+      void solve(string str,string &ans,int indx,int k){
+        ans=max(ans,str);
+        if(k==0)return;
+        for(int i=indx;i<str.size()-1;i++){
+            for(int j=i+1;j<str.size();j++){
+                if(str[j]>str[i]){
+                    swap(str[i],str[j]);
+                    solve(str,ans,i+1,k-1);
+                    swap(str[i],str[j]);
+                }
+            }
+        }
+        
+    }
+    public:
+    //Function to find the largest number after k swaps.
+    string findMaximumNum(string str, int k)
+    {
+       // code here.
+          string ans="";
+       solve(str,ans,0,k);
+       return ans;
+       
+    }
+};

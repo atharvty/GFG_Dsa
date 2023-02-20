e675bf3947a73f0f48997344a66540ec0ab79e30
ATHARV TYAGI <73381314+atharvty@users.noreply.github.com>
2023-02-20 14:48:37 +0530

    Hard on GFG

---
+++ FindMedianInAStream.cpp
@@ -0,0 +1,49 @@
+class Solution
+{
+    public:
+    priority_queue<double>s;
+    priority_queue<double,vector<double>,greater<double>>g;
+    //Function to insert heap.
+    void insertHeap(int &x)
+    {
+        if(s.size()==0||s.top()>x)
+        {
+            s.push(x);
+        }
+        else
+        {
+            g.push(x);
+        }
+        balanceHeaps();
+    }
+    
+    //Function to balance heaps.
+    void balanceHeaps()
+    {
+        if(s.size()>g.size()+1)
+        {
+            g.push(s.top());
+            s.pop();
+        }
+        else if(g.size()>s.size())
+        {
+            s.push(g.top());
+            g.pop();
+        }
+    }
+    
+    //Function to return Median.
+    double getMedian()
+    {
+        double med;
+        if(g.size()==s.size())
+        {
+            med=(s.top()+g.top())/2.0;
+        }
+        else
+        {
+            med=s.top();
+        }
+        return med;
+    }
+};

e028905acf33fb8791b1e78d95572e4f35af0ffd
ATHARV TYAGI <73381314+atharvty@users.noreply.github.com>
2023-04-17 23:52:36 +0530

    Easy on GFG

---
+++ Reverse a string using Stack.cpp
@@ -0,0 +1,19 @@
+
+
+//return the address of the string
+char* reverse(char *S, int len)
+{
+    //code here
+    stack<char>st;
+    for(int i=0;i<len;i++)
+    {
+        st.push(S[i]);
+    }
+    int i=0;
+    while(!st.empty())
+    {
+        S[i++]=st.top();
+        st.pop();
+    }
+    return S;
+}
